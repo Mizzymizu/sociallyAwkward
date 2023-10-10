c1e814f18ed3baf2a9595cba2e604e9461344f00
unknown <danleviobello@gmail.com>
2023-10-09 20:11:46 -0500

    Worked on the Users Model

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,43 @@
-const { Schema, Model } = require('mongoose');
+const { Schema, Model } = require("mongoose");
+const thoughtSchema = require("./Thoughts");
 
 // User Schema
-const userSchema = new Schema();
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // Mongoose email validation
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: 'Email validation has failed, my friend.'
+      }
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId, ref: 'thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId, ref: 'users'
+    }]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Virtual to get total friend count
+postSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
 
 const Users = model('users', userSchema);
 
