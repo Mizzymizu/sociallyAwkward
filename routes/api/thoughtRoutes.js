80fefddc221c406470495aa23e3cf00d0e23962f
unknown <danleviobello@gmail.com>
2023-10-15 15:32:44 -0500

    Updated thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,6 +10,16 @@ const {
     unreact
 } = require('../../controllers/thoughtController')
 
+// /api/thoughts
+router.route('/').get(getThoughts).post(think);
+
+// /api/thought/:thoughtId
+router.route('/:thoughtId').get(getThoughtById).put(rethink).delete(unthink);
+
+// /api/thought/:thoughtId/reaction
+router.route('/thoughtId/reaction').post(react).delete(unreact);
+
+
 
 
 module.exports = router;
